c55d80225322f3b6a8a06b53fe595cf0fc521a81
Shubham Mehta <48021023+shubham64mehta@users.noreply.github.com>
2021-06-10 14:21:36 +0530

    Alter commands

---
+++ Alter table.sql
@@ -0,0 +1,8 @@
+Add Column: 
+Alter table CUSTOMER add (email varchar(55)); 
+Modify Column:
+Alter table Menu modify Name varchar(80);
+Drop Column:
+Alter table Customer drop column email;
+Drop table: 
+Drop table CASHIER;
